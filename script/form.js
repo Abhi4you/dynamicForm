6c11acf978d382976f50e03768d39e3c54916f1c
Abhi4you <abhirjit2010@gmail.com>
2017-03-19 17:38:01 +0530

    Update form.js

--- script/form.js
+++ script/form.js
@@ -1,1 +1,21 @@
+var myForm = angular.module("form",[]);
 
+myForm.controller("formController", function( $scope,$http ){
+	
+	 $scope.getForm = function() {
+		 
+		 $scope.clicked=function(option){
+		     console.log(option)
+		   }
+		 
+		 $scope.selectField = "";
+		var formdata =  $http.get("https://randomform.herokuapp.com/")
+			.then(function(response){ 
+		  		$scope.formDatas = response.data.data;
+		  		console.log($scope.formDatas);
+		  		
+		  	});
+		
+		};
+	    
+});
